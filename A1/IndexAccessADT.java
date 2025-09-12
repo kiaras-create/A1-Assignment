44b88ddd1ef8a5b7e7ff627d087011d39f6706e0
kiaras-create <ksunilsingh@smith.edu>
2025-09-12 14:11:20 -0400

    updated interface

--- A1/IndexAccessADT.java
+++ A1/IndexAccessADT.java
@@ -32,6 +32,78 @@ interface IndexAccessADT <T> {
      */
     public T lenArray(T[] arr);
 
+    /**
+     * @param index of placement where user wants to add an element
+     * @param element that will be added to specified index 
+     * Inserts element in specified index and shifts all other elements accordingly
+     * @return updated dynamic array
+     */
+    public T addElement(int index, T element);
+
+
+    /**
+     * 
+     * @param index of element user wants to remove
+     * @param element that will be removed at the specified index
+     * Removes element at specified index and shifts all other elements left 
+     * @return updated dynamic array
+     */
+        public T removeElement(int index, T element);
+
+    /**
+     * @param dynamicArray that will be concatenated to the end of the array
+     * Will add a the dynamic array to the end of the array
+     * @return updated dynamic array
+     */
+    public T appendArray(T dynamicArray);
+
+    /**
+     * 
+     * @param index of the list
+     * @param dynamicArray that will be removed from the dynamic array
+     * Will remove a dynamic array from the specified indices
+     * @return updated dynamic array
+     */
+    public T insertArray(T dynamicArray, int index);
+
+    /**
+     * 
+     * @param startIndex index of the starting point
+     * @param endIndex index of the end
+     * @return copy of the list from starting point to ending point
+     */
+    public T subList(int startIndex, int endIndex);
+
+    /**
+     * 
+     * @param startIndex
+     * @param endIndex
+     * @return updated dynamic array
+     */
+    public T deleteList(int startIndex, int endIndex);
+
+    /**
+     * 
+     * @param fromIndex
+     * @param toIndex
+     * @return new dynamic array
+     */
+    public T extractArray(int fromIndex, int toIndex);
+
+
+    
+
+
+
+
+
+
+
+
+
+    /**
+     * 
+     */
 
 
 
